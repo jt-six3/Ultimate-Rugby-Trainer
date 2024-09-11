a45769f1253f2419de9243fc28abf26027b177e9
jtsix-3 <jttown25@g.holycross.edu>
2024-09-11 18:32:54 -0400

    Initial Commit

---
+++ Ultimate Rugby Trainer/Ultimate_Rugby_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_Rugby_TrainerApp.swift
+//  Ultimate Rugby Trainer
+//
+//  Created by Joseph Townsend on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_Rugby_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
